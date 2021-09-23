6d24d7aa8ba0829f47f23de447720a8cadf21d7e
kirimaru <nainaistar@gmail.com>
2021-09-23 05:01:41 +0000

    [Add] parameterized

--- domain/fizzBuzz_test.go
+++ domain/fizzBuzz_test.go
@@ -10,3 +10,23 @@ func TestFizzBuzz(t *testing.T) {
         t.Fatal("failed test", ans, "1")
     }
 }
+
+// 参考
+// https://elliotchance.medium.com/go-data-driven-or-parameterized-tests-cd2b62b21c8a
+
+func TestParameterizedTest(t *testing.T) {
+	parameters := []struct {
+		input int
+        expected string
+	}{
+		{1, "1"},
+	}
+
+	for i := range parameters {
+		actual := fizzbuzz(parameters[i].input)
+		if actual != parameters[i].expected {
+			t.Logf("expected%s: , actual:%s", parameters[i].expected, actual)
+			t.Fail()
+		}
+	}
+}
